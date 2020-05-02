ad60c6451d9c8b25d92d856fc394e6d56017d00c
mhmudDev <44923654+mhmudDev@users.noreply.github.com>
2020-05-02 17:43:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "M"
-var adminprefix = 'M'
+var prefix = "2"
+var adminprefix = '2'
 
 
 //bc
 
 client.on("message", message => {
-    if (message.content.startsWith("Mobc")) {
+    if (message.content.startsWith("2obc")) {
                  if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' ');
@@ -23,7 +23,7 @@ client.on("message", message => {
 //bc online
 
 
-  var prefix = "M";
+  var prefix = "2";
 
   client.on("message", message => {
   
@@ -41,7 +41,7 @@ client.on("message", message => {
 
 client.on('message', message => {
     var  user = message.mentions.users.first() || message.author;
-if (message.content.startsWith("Bavatar")) {
+if (message.content.startsWith("2avatar")) {
 message.channel.send(`This avatar For ${user} link : ${user.avatarURL}`);
 }
 });
@@ -56,7 +56,7 @@ client.on('ready',  () => {
   client.on('message', message => {
     if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('Badminbc')){
+if (message.content.startsWith('2adminbc')){
 if(!message.author.id === '476185102922285066') return;
 message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
 client.users.forEach(m =>{
@@ -66,23 +66,23 @@ m.sendMessage(args)
 });
 
   client.on('message', msg => {
-    if(msg.content === 'Mhelp')
+    if(msg.content === '2help')
     msg.reply('Check Your DM :white_check_mark:')
   });
   
   
   client.on("message", message => {
-    if (message.content === "Bhelp") {
+    if (message.content === "2help") {
      const embed = new Discord.RichEmbed() 
          .setColor("#00FF00")
          .setThumbnail(message.author.avatarURL)
          .setDescription(`**Help|هيلب
 
-       Mobc | لأرسال برود كاست للكل
+       2obc | لأرسال برود كاست للكل
 
-       Mbc  |  لأرسال برود كاست للأونلاين
+       2bc  |  لأرسال برود كاست للأونلاين
 
-       Madminbc | برودكاست عادي
+       2adminbc | برودكاست عادي
 
        ** `)
    message.author.sendEmbed(embed)
